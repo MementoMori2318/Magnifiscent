8082b696ab8b5bade25b34a8c2ca604cbdf1d1a6
MementoMori2318 <johnrogerargarin@gmail.com>
2023-04-12 11:21:34 +0800

    wew

--- component.php
+++ component.php
@@ -10,9 +10,9 @@ function component($productname, $productprice, $productimage, $productid){
                 <div class='card-body'>
                     <h5 class='title'>$productname</h5>
                     <h5>
-                        <span class='price'>₱$productprice</span>
+                        <span class='price'>₱$productprice.00</span>
                     </h5>
-                    <button class='btn' type='submit' name='add'>Add to Cart<i class='fas fa-shopping-cart'></i></button>
+                    <button class='btn' type='submit' name='add'>Add to Cart  <i class='fas fa-shopping-cart'></i></button>
                     <input type='hidden' name='product_id' value='$productid'/>
                 </div>
             </div>

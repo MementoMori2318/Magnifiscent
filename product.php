4302a15c2f911fb1a01775339c27d39202dda4d6
MementoMori2318 <johnrogerargarin@gmail.com>
2023-04-17 18:07:09 +0800

    wewwew

--- product.php
+++ product.php
@@ -38,38 +38,27 @@
         }
     }
    
-?>
-
-<!DOCTYPE html>
+?><!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style.css" href="styles.css?version=51">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link href="https://fonts.googleapis.com/css2?family=Libre+Baskerville&display=swap" rel="stylesheet">
-
-    <title>Magnifiscent</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title></title>
 </head>
 <body>
-
-    <?php require("header.php")?>
-    
-    <!-- START HERO -->
-    <div class="hero-container">
-        <div class="hero-title">
-            <h1>MagnifiScent</h1>
-            <h5>Perfume is a journey that starts on the skin and continues to the soul.</h5>
-            <a href="product.php">EXPLORE OUR SCENT        <i class="fa-solid fa-arrow-right"></i></a>
-        </div>
-        <div class="hero-img">
-
+    <!-- START Product -->
+    <div class="container">
+        <div class="row">
+           <?php
+            $result = $database->getData(5);
+            while($row = mysqli_fetch_assoc($result)){
+                component($row['product_name'],$row['product_price'],$row['product_image'],$row['id']); 
+            } 
+           ?>
         </div>
     </div>
-    <!-- ENd HERO -->
-
-    
+    <!--END Product -->
 </body>
 </html>

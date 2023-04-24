aee30bba4735e652ae2d8ee5c758e7dc628923ca
MementoMori2318 <johnrogerargarin@gmail.com>
2023-04-24 15:50:29 +0800

    wew

--- includes/cart.inc.php
+++ includes/cart.inc.php
@@ -1,5 +1,36 @@
 <?php 
 ob_start();
+// function addToCart($conn) {
+//     if(isset($_POST['add_to_cart'])){
+//         $product_id = $_POST['add_to_cart'];
+//         $user_id = $_SESSION['userid'];
+//         $date = date('Y-m-d H:i:s');
+       
+//         $sql = "SELECT * FROM cartdb WHERE product_id=? AND users_id=?";
+//         $stmt = $conn->prepare($sql);
+//         $stmt->bind_param("ii", $product_id, $user_id);
+//         $stmt->execute();
+//         $result = $stmt->get_result();
+
+//         $num_rows = $result->num_rows;
+
+//         if($num_rows > 0){
+//             // Product already exists in cart, update quantity
+//             $row = $result->fetch_assoc();
+//             $new_quantity = $row['product_quantity'] + 1;
+//             $sql = "UPDATE cartdb SET product_quantity=?, date=? WHERE id=?";
+//             $stmt = $conn->prepare($sql);
+//             $stmt->bind_param("iss", $new_quantity, $date, $row['id']);
+//             $stmt->execute();
+//         } else {
+//             // Product doesn't exist in cart, insert new row
+//             $sql = "INSERT INTO cartdb (product_id, product_quantity, users_id, date) VALUES (?, 1, ?, ?)";
+//             $stmt = $conn->prepare($sql);
+//             $stmt->bind_param("iii", $product_id, $user_id, $date);
+//             $stmt->execute();
+//         }
+//     }
+// }
 function addToCart($conn) {
     if(isset($_POST['add_to_cart'])){
         $product_id = $_POST['add_to_cart'];
@@ -17,19 +48,38 @@ function addToCart($conn) {
         if($num_rows > 0){
             // Product already exists in cart, update quantity
             $row = $result->fetch_assoc();
-            $new_quantity = $row['quantity'] + 1;
-            $sql = "UPDATE cartdb SET quantity=?, date=? WHERE id=?";
+            $new_quantity = $row['product_quantity'] + 1;
+            $sql = "UPDATE cartdb SET product_quantity=?, date=? WHERE id=?";
             $stmt = $conn->prepare($sql);
             $stmt->bind_param("iss", $new_quantity, $date, $row['id']);
             $stmt->execute();
         } else {
             // Product doesn't exist in cart, insert new row
-            $sql = "INSERT INTO cartdb (product_id, quantity, users_id, date) VALUES (?, 1, ?, ?)";
+            $sql = "INSERT INTO cartdb (product_id, product_quantity, users_id, date) VALUES (?, 1, ?, ?)";
             $stmt = $conn->prepare($sql);
             $stmt->bind_param("iii", $product_id, $user_id, $date);
             $stmt->execute();
         }
-    }
+
+        // Update cart_total in cartdb
+        $sql = "SELECT SUM(product_quantity) AS total FROM cartdb WHERE users_id=?";
+        $stmt = $conn->prepare($sql);
+        $stmt->bind_param("i", $user_id);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $row = $result->fetch_assoc();
+        $cart_total = $row['total'];
+        $sql = "UPDATE cartdb SET cart_total=? WHERE users_id=?";
+        $stmt = $conn->prepare($sql);
+        $stmt->bind_param("ii", $cart_total, $user_id);
+        $stmt->execute();
+
+        // Update cart_total in customer table
+        $sql = "UPDATE customer SET cart_total=? WHERE usersid=?";
+        $stmt = $conn->prepare($sql);
+        $stmt->bind_param("ii", $cart_total, $user_id);
+        $stmt->execute();
+        }
 }
 
 function getProduct($conn){
@@ -45,7 +95,7 @@ function getProduct($conn){
                 </div>
                 <div class='card-body'>
                     <h5 class='title'>" . $row['product_name'] . "</h5>
-                    <h5 class='price'>" . $row['product_price'] . ".00</h5>
+                    <h5 class='price'>₱" . $row['product_price'] . ".00</h5>
                 </div>
                     <div class='btn-container'>
                         <button class='btn' type='submit' name='buy'>Buy Now</button>

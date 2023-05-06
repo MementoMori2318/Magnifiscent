8cf155e2550aad278302c7ad1acf80993373e326
MementoMori2318 <johnrogerargarin@gmail.com>
2023-05-06 09:05:39 +0800

    wew

--- magnifiscent.sql
+++ magnifiscent.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.0
+-- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 23, 2023 at 11:08 AM
--- Server version: 10.4.24-MariaDB
--- PHP Version: 8.1.6
+-- Generation Time: May 06, 2023 at 03:04 AM
+-- Server version: 10.4.28-MariaDB
+-- PHP Version: 8.2.4
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -24,26 +24,17 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `cartdb`
+-- Table structure for table `cart`
 --
 
-DROP TABLE IF EXISTS `cartdb`;
-CREATE TABLE `cartdb` (
+CREATE TABLE `cart` (
   `id` int(11) NOT NULL,
   `product_id` varchar(25) NOT NULL,
-  `quantity` int(11) DEFAULT NULL,
+  `product_quantity` int(11) DEFAULT NULL,
   `users_id` int(11) DEFAULT NULL,
+  `cart_total` int(11) NOT NULL,
   `date` date DEFAULT current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Dumping data for table `cartdb`
---
-
-INSERT INTO `cartdb` (`id`, `product_id`, `quantity`, `users_id`, `date`) VALUES
-(76, '1', 30, 3, '2023-04-23'),
-(77, '5', 10, 3, '2023-04-23'),
-(78, '4', 5, 3, '2023-04-23');
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
@@ -51,23 +42,23 @@ INSERT INTO `cartdb` (`id`, `product_id`, `quantity`, `users_id`, `date`) VALUES
 -- Table structure for table `customer`
 --
 
-DROP TABLE IF EXISTS `customer`;
 CREATE TABLE `customer` (
   `usersID` int(11) NOT NULL,
   `usersName` varchar(255) NOT NULL,
   `usersEmail` varchar(255) NOT NULL,
   `usersUid` varchar(255) NOT NULL,
   `usersPwd` varchar(255) NOT NULL,
-  `usersAddress` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  `usersAddress` varchar(255) NOT NULL,
+  `cart_total` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `customer`
 --
 
-INSERT INTO `customer` (`usersID`, `usersName`, `usersEmail`, `usersUid`, `usersPwd`, `usersAddress`) VALUES
-(2, 'jr', 'jojo@gmail.com', 'demon', '$2y$10$/fd2OBgRwQsY1QvIDoyWL.9r520xFbjiJN/mtCNkX7p2fWJInwMMe', 'tres reyes bato'),
-(3, 'Mario', 'mario@gmail.com', 'SuperMario', '$2y$10$yheSrl9fSe76TKqnIcmgROAo076/s3t/UqywwkPhsVxSXIIoGumya', 'tres reyes bato');
+INSERT INTO `customer` (`usersID`, `usersName`, `usersEmail`, `usersUid`, `usersPwd`, `usersAddress`, `cart_total`) VALUES
+(1, 'Jojo', 'jojo@gmail.com', 'StarPlatinum', '$2y$10$/MuHOrMm4WgboZAlvAHSQOR9ZZGyyITCuWd1CY8JtPU2M7FW2dVzW', 'tres reyes bato', 0),
+(2, 'John Roger ', 'johnrogerargarin@yahoo.com', 'Joro', '$2y$10$EK2NPyFVDFU2qpUoCe4v8uoNpVT9J8AITuSpl9BFKRwVfhyD6f.Sq', 'tres reyes bato', 20);
 
 -- --------------------------------------------------------
 
@@ -75,14 +66,13 @@ INSERT INTO `customer` (`usersID`, `usersName`, `usersEmail`, `usersUid`, `users
 -- Table structure for table `products`
 --
 
-DROP TABLE IF EXISTS `products`;
 CREATE TABLE `products` (
   `id` int(11) NOT NULL,
   `product_name` varchar(25) NOT NULL,
-  `product_price` float DEFAULT NULL,
-  `product_image` varchar(100) DEFAULT NULL,
+  `product_price` float NOT NULL,
+  `product_image` varchar(100) NOT NULL,
   `product_stock` int(100) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `products`
@@ -93,16 +83,21 @@ INSERT INTO `products` (`id`, `product_name`, `product_price`, `product_image`, 
 (2, 'Pure Poison Dior', 969, 'image/6.png', 20),
 (3, 'Bright Crystal', 1000, 'image/9.png', 20),
 (4, 'Crystal Nior', 599, 'image/10.png', 3),
-(5, 'Black Opium', 1599, 'image/14.png', 20);
+(5, 'Black Opium', 1599, 'image/14.png', 20),
+(6, 'Dior Jadore', 1299, 'image/1.png', 20),
+(7, 'Gabbana Light', 2469, 'image/2.png', 20),
+(8, 'Gucci Bloom', 1200, 'image/3.png', 20),
+(9, 'Marc Jacobs Daisy', 1599, 'image/4.png', 3),
+(10, 'Calvin Klein Euphoria', 1299, 'image/5.png', 20);
 
 --
 -- Indexes for dumped tables
 --
 
 --
--- Indexes for table `cartdb`
+-- Indexes for table `cart`
 --
-ALTER TABLE `cartdb`
+ALTER TABLE `cart`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -122,22 +117,22 @@ ALTER TABLE `products`
 --
 
 --
--- AUTO_INCREMENT for table `cartdb`
+-- AUTO_INCREMENT for table `cart`
 --
-ALTER TABLE `cartdb`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=79;
+ALTER TABLE `cart`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=110;
 
 --
 -- AUTO_INCREMENT for table `customer`
 --
 ALTER TABLE `customer`
-  MODIFY `usersID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `usersID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `products`
 --
 ALTER TABLE `products`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;

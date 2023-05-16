26130601d1f166eb972549be4b69d9e356d1f4f0
MementoMori2318 <johnrogerargarin@gmail.com>
2023-05-16 20:06:45 +0800

    wew

--- includes/cart.inc.php
+++ includes/cart.inc.php
@@ -45,24 +45,34 @@ function addToCart($conn) {
                 $_SESSION['cart_total'] = $cart_total;
 
                 // Update cart total using AJAX
-                echo "<script>
-                function updateCartTotal() {
-                    var xhttp = new XMLHttpRequest();
-                    xhttp.onreadystatechange = function() {
-                        if (this.readyState == 4 && this.status == 200) {
-                            document.getElementById('cart-total').innerHTML = this.responseText;
-                        }
-                    };
-                    xhttp.open('GET', 'get_cart_total.php', true);
-                    xhttp.send();
-                }
+                // echo "<script>
+                // function updateCartTotal() {
+                //     var xhttp = new XMLHttpRequest();
+                //     xhttp.onreadystatechange = function() {
+                //         if (this.readyState == 4 && this.status == 200) {
+                //             document.getElementById('cart-total').innerHTML = this.responseText;
+                //         }
+                //     };
+                //     xhttp.open('GET', 'get_cart_total.php', true);
+                //     xhttp.send();
+                // }
 
-                // Call updateCartTotal() on page load
-                updateCartTotal();
+                // // Call updateCartTotal() on page load
+                // updateCartTotal();
 
-                // Call updateCartTotal() every 5 seconds (5000 milliseconds)
-                setInterval(updateCartTotal, 1000);";
-                echo "</script>";
+                // // Call updateCartTotal() every 5 seconds (5000 milliseconds)
+                // setInterval(updateCartTotal, 1000);";
+                // echo "</script>";
+                ?>
+                <script>
+                    swal({
+                    title: "Greate!",
+                    text: "Successfully added to the cart!",
+                    icon: "success",
+                    button: "Aww yiss!",
+                    });
+                </script>
+                <?php
             }
         } else {
             header("Location: login.php");
@@ -90,7 +100,7 @@ function getProduct($conn){
                 <div class='card-footer'>
                     <span class='text-title'>₱" . $row['product_price'] . ".00</span>
                 <form action='" . addToCart($conn) . "' method='POST'>
-                    <button class='card-button' type='submit' name='add_to_cart' value='" . $row['id'] . "'>
+                    <button id='addButton' class='card-button' type='submit' name='add_to_cart' value='" . $row['id'] . "'>
                         <svg class='svg-icon' viewBox='0 0 20 20'>
                         <path d='M17.72,5.011H8.026c-0.271,0-0.49,0.219-0.49,0.489c0,0.271,0.219,0.489,0.49,0.489h8.962l-1.979,4.773H6.763L4.935,5.343C4.926,5.316,4.897,5.309,4.884,5.286c-0.011-0.024,0-0.051-0.017-0.074C4.833,5.166,4.025,4.081,2.33,3.908C2.068,3.883,1.822,4.075,1.795,4.344C1.767,4.612,1.962,4.853,2.231,4.88c1.143,0.118,1.703,0.738,1.808,0.866l1.91,5.661c0.066,0.199,0.252,0.333,0.463,0.333h8.924c0.116,0,0.22-0.053,0.308-0.128c0.027-0.023,0.042-0.048,0.063-0.076c0.026-0.034,0.063-0.058,0.08-0.099l2.384-5.75c0.062-0.151,0.046-0.323-0.045-0.458C18.036,5.092,17.883,5.011,17.72,5.011z'></path>
                         <path d='M8.251,12.386c-1.023,0-1.856,0.834-1.856,1.856s0.833,1.853,1.856,1.853c1.021,0,1.853-0.83,1.853-1.853S9.273,12.386,8.251,12.386z M8.251,15.116c-0.484,0-0.877-0.393-0.877-0.874c0-0.484,0.394-0.878,0.877-0.878c0.482,0,0.875,0.394,0.875,0.878C9.126,14.724,8.733,15.116,8.251,15.116z'></path>
---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById('addButton').addEventListener('click',
+function() {
+    document.querySelector('.modal').style.display = 'flex';
+});
